44313cb841a8c4b307d0664e012b584b07a748be
Mumu <jbzbajhabswi64281@gmail.com>
2025-10-30 18:34:12 +0700

    Delete output.lua

--- output.lua
+++
@@ -1,8 +0,0 @@
---[[
-	Code generated using github.com/Herrtt/luamin.js
-	An open source Lua beautifier and minifier.
---]]
-
-
-
-local a=loadstring(game:HttpGet("https://sirius.menu/rayfield"))()local a=a:CreateWindow({Name="Luby Hub | Game Loader",LoadingTitle="Luby Hub",LoadingSubtitle="by Yuzuhaแฟนกู❤🎧"})local a=a:CreateTab("Select Game","gamepad-2")a:CreateButton({Name="Sakura Stand",Callback=function()loadstring(game:HttpGet("https://raw.githubusercontent.com/WAEXETH/RubyHub.luau/refs/heads/main/LubyHub.lua"))()end})a:CreateButton({Name="The Strongest Battlegrounds",Callback=function()loadstring(game:HttpGet("https://raw.githubusercontent.com/WAEXETH/RubyHub.luau/refs/heads/main/Kikik.lua"))()end})a:CreateButton({Name="Heroes Battlegrounds",Callback=function()loadstring(game:HttpGet("https://raw.githubusercontent.com/WAEXETH/RubyHub.luau/refs/heads/main/HB.lua"))()end})a:CreateButton({Name="Rider World",Callback=function()loadstring(game:HttpGet("https://raw.githubusercontent.com/WAEXETH/RubyHub.luau/refs/heads/main/riderworld.lua"))()end})a:CreateButton({Name="ProjectSmash",Callback=function()loadstring(game:HttpGet("https://raw.githubusercontent.com/WAEXETH/RubyHub.luau/refs/heads/main/ProjectSmash.lua"))()end})loadstring(game:HttpGet("https://raw.githubusercontent.com/WAEXETH/RubyHub.luau/refs/heads/main/kuy.lua"))()
